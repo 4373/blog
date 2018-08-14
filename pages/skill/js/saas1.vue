52fe536859e34eb37e295be2407e8b2dfc259fc6
me <mh4373@163.com>
2018-08-15 01:42:43 +0800

    add menu example

--- pages/skill/js/saas1.vue
+++ pages/skill/js/saas1.vue
@@ -7,7 +7,31 @@
   .view p {
     margin: 2px 0;
   }
-
+  .box{
+    width: 200px;
+    box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
+    padding: 8px;
+    border-radius: 8px;
+  }
+  .parent{
+    color: #999;
+    font-size: 12px;
+    cursor: pointer;
+  }
+  .icon{
+    margin-right: 4px;
+  }
+  .children{
+    /* padding-left: 10px; */
+    color: #666;
+  }
+  .child{
+    padding: 6px 16px;
+    cursor: pointer;
+  }
+  .child:hover{
+    background: #f8f8f8;
+  }
 </style>
 <template>
   <div>
@@ -44,20 +68,22 @@
               path: '/role', 
               name: '角色管理', 
               component: { render(h) {return h('router-view')}}, 
+              icon: 'icon-life',
               children: [
-                {path: '/role/list', component: RoleList},
-                {path: '/role/deal', component: RoleDeal},
-                {path: '/role/detail', component: RoleDetail},
+                {path: '/role/list', component: RoleList, name: '角色列表', hidden: true},
+                {path: '/role/deal', component: RoleDeal, name: '角色处理'},
+                {path: '/role/detail', component: RoleDetail, name: '角色详情'},
               ]
             },
             { 
               path: '/account', 
               name: '帐号管理', 
               component: { render(h) {return h('router-view')}}, 
+              icon: 'icon-user',
               children: [
-                {path: '/account/list', component: AccountList},
-                {path: '/account/deal', component: AccountDeal},
-                {path: '/account/detail', component: AccountDetail},
+                {path: '/account/list', component: AccountList, name: '账号列表'},
+                {path: '/account/deal', component: AccountDeal, name: '账号处理'},
+                {path: '/account/detail', component: AccountDetail, name: '账号详情'},
               ]
             },
             ...otherMenu
@@ -76,7 +102,28 @@
         </code>
       </pre>
     </div>
-    <p>导出的router不用多说， 是vue实例化所需要的路由对象，而routes的话， 在本项目中主要用来遍历菜单的侧边栏，结合后端传过来的权限列表，可以过滤出该用户的权限菜单，这个会在权限篇具体讨论。</p>
+    <p>导出的router不用多说， 是vue实例化所需要的路由对象，而routes的话， 在本项目中主要用来遍历菜单的侧边栏，对于每一个item，你可以加一些你需要的字段，比如该菜单栏的字体图标，亦比如你目前在开发一个新的模块，但你现在这个入口不想对外公布，你所需要做的就是加一个hidden字段就可以了，一切需求就由你自由发挥了。</p>
+    <p>如果菜单template是这样子的：</p>
+    <pre class="language-markup">
+      <code class="language-markup" v-text='html'>
+        
+      </code>
+    </pre>
+    <p>那么最终渲染出来就是这个样子了：</p>
+    <div class="box">
+      <div class="parent" v-for = '(item, key) in routes' :key='key'>
+        <div class="parent-name">
+          <svg class="icon" aria-hidden="true">
+              <use :xlink:href="'#' + item.icon"></use>
+          </svg>
+          <span>{{item.name}}</span>
+          <div class="children">
+            <p class="child" v-for = '(i, k) in item.children' :key='k' v-if='!i.hidden' :title='i.path'>{{i.name}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p>导出的routes结合后端传过来的权限列表，可以过滤出该用户的权限菜单，这个会在权限篇具体讨论。</p>
     <p>根据router.js内容生成的页面结构如下：</p>
     <div class="view ">
       <p class="bg-primary pa-1">主页</p>
@@ -93,11 +140,53 @@
       <p class="bg-primary pa-1">500</p>
     </div>
     <p>蓝色是顶层页面， 主要包括登陆页， 404， 500 等不需要登陆的页面，以及项目的主页home，项目的主要功能都写在这里面。红色就是菜单项，里面会包含该菜单项的功能页面，也就是灰色部分。灰色部分不会出现在菜单栏，同通过点击红色项redirect到该项下面的列表页，该菜单项的其他部分的入口全在列表页。</p>
+
   </div>
 </template>
 <script>
   export default {
     layout: 'home',
+    data() {
+      return {
+        routes: [
+            { 
+              path: '/role', 
+              name: '角色管理', 
+              component: { render(h) {return h('router-view')}}, 
+              icon: 'icon-life',
+              children: [
+                {path: '/role/list', component: 'RoleList', name: '角色列表', hidden: true},
+                {path: '/role/deal', component: 'RoleDeal', name: '角色处理'},
+                {path: '/role/detail', component: 'RoleDetail', name: '角色详情'},
+              ]
+            },
+            { 
+              path: '/account', 
+              name: '帐号管理', 
+              component: { render(h) {return h('router-view')}}, 
+              icon: 'icon-user',
+              children: [
+                {path: '/account/list', component: 'AccountList', name: '账号列表'},
+                {path: '/account/deal', component: 'AccountDeal', name: '账号处理'},
+                {path: '/account/detail', component: 'AccountDetail', name: '账号详情'},
+              ]
+            }
+          ],
+          html: `
+          <div class="parent" v-for = '(item, key) in routes' :key='key'>
+            <div class="parent-name">
+              <svg class="icon" aria-hidden="true">
+                  <use :xlink:href="'#' + item.icon"></use>
+              </svg>
+              <span>{{item.name}}</span>
+              <div class="children">
+                <p class="child" v-for = '(i, k) in item.children' :key='k' v-if='!i.hidden' :title='i.path'>{{i.name}}</p>
+              </div>
+            </div>
+          </div>
+          `
+      }
+    },
     mounted() {
 
     }
